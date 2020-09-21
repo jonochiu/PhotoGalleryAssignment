c46a7ebf162bada5d45ad58a24d29d1a9c292295
OPWonShinobi <al28707b>
2020-09-20 19:01:18 -0700

    Cleaned up duplicate code. Now clicking date editText brings up date picker, and saves to correct field Keyboard is now hidden after date select.

--- app/src/main/java/com/example/photogalleryassignment/SearchActivity.java
+++ app/src/main/java/com/example/photogalleryassignment/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.example.photogalleryassignment;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -21,24 +24,48 @@ public class SearchActivity extends AppCompatActivity{
 
     private EditText startDateDispl;
     private EditText endDateDispl;
-    private DatePickerDialog.OnDateSetListener startDateListener;
-    private DatePickerDialog.OnDateSetListener endDateListener;
-    private View.OnClickListener calendarClickListener = new View.OnClickListener() {
+    private DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onClick(View view) {
-            showCalendar();
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            setDate(startDateDispl, year, month, day);
         }
     };
-    private View.OnFocusChangeListener calendarChangeListener = new View.OnFocusChangeListener() {
+    private DatePickerDialog.OnDateSetListener endDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            setDate(endDateDispl, year, month, day);
+        }
+    };
+    private View.OnClickListener startCalendarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            showCalendar(startDateListener);
+        }
+    };
+    private View.OnClickListener endCalendarClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            showCalendar(endDateListener);
+        }
+    };
+    private View.OnFocusChangeListener startCalendarChangeListener = new View.OnFocusChangeListener() {
         @Override
         public void onFocusChange(View view, boolean hasFocus) {
             if (hasFocus) {
-                showCalendar();
+                showCalendar(startDateListener);
+            }
+        }
+    };
+    private View.OnFocusChangeListener endCalendarChangeListener = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View view, boolean hasFocus) {
+            if (hasFocus) {
+                showCalendar(endDateListener);
             }
         }
     };
 
-    private void showCalendar() {
+    private void showCalendar(DatePickerDialog.OnDateSetListener dateSetListener) {
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
@@ -47,12 +74,19 @@ public class SearchActivity extends AppCompatActivity{
         DatePickerDialog dialog = new DatePickerDialog(
                 SearchActivity.this,
                 android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                startDateListener,
+                dateSetListener,
                 year,month,day);
         dialog.getWindow().setBackgroundDrawable((new ColorDrawable(Color.TRANSPARENT)));
         dialog.show();
     }
-
+    private void setDate(EditText editText, int year, int month, int day) {
+        month = month+1;
+        String date = year+"-"+month+"-"+day;;
+        editText.setText(date);
+        //hide keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getApplicationWindowToken(), 0);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,28 +96,13 @@ public class SearchActivity extends AppCompatActivity{
         endDateDispl = (EditText)findViewById(R.id.endDate);
 
         //Listener for setting startdate
-        startDateDispl.setOnClickListener(calendarClickListener);
-        startDateDispl.setOnFocusChangeListener(calendarChangeListener);
-        startDateListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month+1;
-                String date = year+"-"+month+"-"+day;;
-                startDateDispl.setText(date);
-            }
-        };
+        startDateDispl.setOnClickListener(startCalendarClickListener);
+        startDateDispl.setOnFocusChangeListener(startCalendarChangeListener);
 
         //Listener for setting enddate
-        endDateDispl.setOnClickListener(calendarClickListener);
-        endDateDispl.setOnFocusChangeListener(calendarChangeListener);
-        endDateListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month = month+1;
-                String date = year+"-"+month+"-"+day;;
-                endDateDispl.setText(date);
-            }
-        };
+        endDateDispl.setOnClickListener(endCalendarClickListener);
+        endDateDispl.setOnFocusChangeListener(endCalendarChangeListener);
+
 //        String[] projection = new String[] {
 //                MediaStore.Images.Media._ID,
 //                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
